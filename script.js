5b2444d9c28f231d06f5018a79b618203ac93236
CooperMoore9 <thefryman9@gmail.com>
2022-07-21 13:19:35 -0500

    Made Calculator

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
 let num1 = '';
 let num2 = '';
+let sign = '';
+let evaluate = '';
 
 function add(num1, num2){
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 };
 
 function subtract(num1, num2){
-    return num1 - num2;
+    return parseInt(num1) - parseInt(num2);
 };
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return parseInt(num1) * parseInt(num2);
 };
 
 function divide(num1, num2){
-    return num1 / num2;
+    return parseInt(num1) / parseInt(num2);
 };
 
-function operate(operator, num1, num2){
-
+function operate(sign, num1, num2){
+    if(sign === '+'){
+        evaluate = add(num1, num2);
+    }else if(sign === '-'){
+        evaluate = subtract(num1, num2);
+    }else if(sign === '*'){
+        evaluate =  multiply(num1, num2);
+    }else if(sign === '/'){
+        evaluate = divide(num1, num2);
+    }
+    
 }
 
 const buttonListener = document.querySelectorAll('button');
     buttonListener.forEach((button) => {
         button.addEventListener('click', () => {
-            if(!isNaN(parseInt(button.id)) ){
-                num1 += button.id;
-                console.log(num1);
+            if(!isNaN(parseInt(button.id)) && sign === ''){
+                num1 += button.textContent;
+            }else if(!isNaN(parseInt(button.id)) && sign !== ''){
+                num2 += button.textContent
+            }else if(button.id === '.' && sign === ''){
+                num1 += '.'
+            }else if(button.id === '.' && sign !== ''){
+               num2 += '.'
+            }else if(button.id === '/'){
+                sign = button.id;
+            }else if(button.id === '*'){
+                sign = button.id;
+            }else if(button.id === '-'){
+                sign = button.id;
+            }else if(button.id === '+'){
+                sign = button.id;
+            }else if(button.id === '='){
+                operate(sign, num1, num2);
+            }else if(button.id === 'clear'){
+                num1 = '';
+                num2 = '';
+                sign = '';
+                evaluate = '';
+                document.getElementById('displayBottom').textContent = `0`;
+            }
+
+            if(evaluate === ''){
+                document.getElementById('displayBottom').textContent = `${num1}` + ` ${sign}` + ` ${num2}`;
+            }else if(evaluate !== ''){
+                document.getElementById('displayBottom').textContent = `${num1}` + ` ${sign}` + ` ${num2}` + ` = ${evaluate}`;
             }
         })
     })
+
